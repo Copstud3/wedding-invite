6be56080be838a8ac1c766c625ab2a350fc7e4cb
Chris <victorchris73@gmail.com>
2025-05-30 18:13:29 +0100

    chore: bug fixes

--- app/_components/rsvp-form.tsx
+++ app/_components/rsvp-form.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { MapPin } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+// import { Label } from "@/components/ui/label";
 import { Calendar } from "@/components/ui/calendar";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { strings } from "../utils/strings";
